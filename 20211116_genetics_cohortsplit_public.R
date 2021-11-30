6b07cbfbd3d83c0be22adce11b6c842473c60c94
acthuesen <37144128+acthuesen@users.noreply.github.com>
2021-11-30 18:08:56 +0100

    Add files via upload

---
+++ 20211116_genetics_cohortsplit_public.R
@@ -0,0 +1,125 @@
+# DEPENDENCIES AND WD ----
+library(readxl)
+library(tidyverse)
+# replace PATH with path
+setwd('PATH/Genetics/SNPs_PRSs_for_datahub/') # where genetic data are and go
+
+# ID MASTER FILE ----
+# Some ID's belong to multiple cohorts. These affiliations are encoded in the ID MASTER FILE
+idcompare <- read_excel('preprocessing/IDcompare_ALD_SIP_ALCO_RFX_PRF_HP.xlsx', sheet='clean', na="NA")
+
+# Four ID's from ALCO cohort are duplicated, remove from the master
+idcompare <- idcompare %>%
+  mutate(Alcochallenge_id = na_if(Alcochallenge_id, 211),
+         Alcochallenge_id = na_if(Alcochallenge_id, 214),
+         Alcochallenge_id = na_if(Alcochallenge_id, 215), 
+         Alcochallenge_id = na_if(Alcochallenge_id, 302))
+
+
+# SINGLE SNPS / SPLIT BY COHORT ----
+# load snp data
+genedata <- read.csv('preprocessing/snps/microbliver_snps.geno.csv', check.names = F) #check names ensures that chr isn't added to numeric vars (i.e. optional)
+
+# fix id col which doesn't have a varname with Carsten's SNP extractor
+colnames(genedata)[1] <- 'id' 
+
+# drop numeric cohort identifiers from the ID name
+# also drop the pnpla3 snp because it's added from the previous imputation batch (see below)
+# also drop the id col again, because we create a better id
+genedata <- genedata %>%
+  mutate(CBMR_ID = str_replace_all(id, "[0-9]+x", "")) %>%
+  select(-`22:44324727:C:G`) %>%
+  select(-id)
+
+# merge ids with data
+genedata <- left_join(genedata, idcompare, by='CBMR_ID')
+
+# get genotyped pnpla3
+pnpla3 <- read.csv('preprocessing/snps/pnpla3snp_1kg.geno.csv', header=T, check.names = F)
+
+# fix id as above and then drop
+# correct spelling mistake in cohort which only exists in this dataset
+colnames(pnpla3)[1] <- 'id' 
+pnpla3 <- pnpla3 %>%
+  mutate(CBMR_ID = str_replace_all(id, "[0-9]+x", "")) %>%
+  mutate(CBMR_ID = gsub('AlCO', 'ALCO', CBMR_ID)) %>%
+  select(-id)
+
+# merge with previous data
+genedata <- full_join(genedata,pnpla3, by='CBMR_ID')
+
+# unite cohorts into one "multiple cohorts" column and then separate into multiple rows
+# remove the sample from SIP that was supposed to be removed during imputation (SIPremove)
+genedata <- genedata %>%
+  mutate(cohort1 = str_replace_all(CBMR_ID, "[0-9]+", "")) %>%
+  mutate(cohort2 = case_when(is.na(Rifaximin_id)==FALSE ~ "RFX")) %>%
+  mutate(cohort3 = case_when(is.na(Alcochallenge_id)==FALSE ~ "ALCO")) %>%
+  mutate(cohort4 = case_when(is.na(Profermin_id)==FALSE ~ "PRF")) %>%
+  unite("cohort", cohort1:cohort4, na.rm=T, remove=FALSE) %>%
+  select(-Rifaximin_id, -Profermin_id, -Alcochallenge_id, -(cohort1:cohort4)) %>%
+  separate_rows(cohort, sep="_") %>%
+  filter(cohort != 'SIPremove')
+
+# split by cohort
+genedata %>% 
+  group_split(cohort) %>% 
+    sapply(., function (x) 
+    write.csv(x, file=paste0("preprocessing/split/extractedsnps_", unique(x$cohort), ".csv"), row.names=F))
+
+
+# PRS/GRS / SPLIT BY COHORT ----
+# Unlike SNP data where multiple SNPs are in one initial file, each PRS/GRS is its own file
+# therefore, load multiple files and merge into one before correcting ids etc and splitting
+# load all prs files
+prss <- list.files("preprocessing/prs", pattern=".prs", ignore.case=T)
+
+# init a new dataframe (so they can merge)
+allprs <- data.frame(IID=character())
+# merge all prs files 
+for(prs in prss){
+  tempdata <- read.table(paste0("preprocessing/prs/", unique(prs)), header=T) #load
+  allprs <- full_join(allprs, tempdata, by="IID") # merge
+  rm(tempdata) # cleanup
+}
+
+# load all grs files
+grss <- list.files("preprocessing/prs", pattern=".txt", ignore.case=T)
+
+# init a new dataframe (so they can merge)
+allgrs <- data.frame(IID=character())
+# merge all prs files 
+for(grs in grss){
+  tempdata <- read.table(paste0("preprocessing/prs/", unique(grs)), header=T, col.names=c('IID',grs)) #load
+  allgrs <- full_join(allgrs, tempdata, by="IID") # merge
+  rm(tempdata) # cleanup
+}
+
+# join prs's and grs's
+allrs <- full_join(allprs, allgrs, by='IID')
+
+# cleanup id
+allrs <- allrs %>%
+  mutate(CBMR_ID = str_replace_all(IID, "[0-9]+x", ""))
+
+# merge ids with data
+allrs <- left_join(allrs, idcompare, by='CBMR_ID')
+
+# unite cohorts and separate
+# also cleanup column names
+allrs <- allrs %>%
+  mutate(cohort1 = str_replace_all(CBMR_ID, "[0-9]+", "")) %>%
+  mutate(cohort2 = case_when(is.na(Rifaximin_id)==FALSE ~ "RFX")) %>%
+  mutate(cohort3 = case_when(is.na(Alcochallenge_id)==FALSE ~ "ALCO")) %>%
+  mutate(cohort4 = case_when(is.na(Profermin_id)==FALSE ~ "PRF")) %>%
+  unite("cohort", cohort1:cohort4, na.rm=T, remove=FALSE) %>%
+  select(-Rifaximin_id, -Profermin_id, -Alcochallenge_id, -IID, -(cohort1:cohort4)) %>%
+  separate_rows(cohort, sep="_") %>%
+  filter(cohort != 'SIPremove') %>%
+  rename_with(~str_remove(., '.txt')) %>%
+  rename_with(~str_remove(., '_MBL_correctX'))
+
+# SPLIT BY COHORT ----
+allrs %>% 
+  group_split(cohort) %>% 
+  sapply(., function (x) 
+    write.csv(x, file=paste0("preprocessing/split/riskscores_", unique(x$cohort), ".csv"), row.names=F))
---
+++ 20211125_olink_cohortsplit_public.R
@@ -0,0 +1,128 @@
+# WHAT THIS DOES ----
+# - Split o-link data by cohort
+# - convert to wide format
+# - correct CBMRID
+# - Add in timepoint/sample variables
+
+# Note that the o-link qc has already been done before this
+# Make sure you're working on the "Global QC" version of the data
+
+# DEPENDENCIES ----
+library(tidyverse)
+library(readxl)
+
+# GET DATA ----
+# replace PATH with the actual path
+olinkdat <- read.csv('PATH/Olink/Global_QC/QC_olink.csv')
+
+# CLEAN UP COHORTS ----
+# HOL-F, HOL-N, Control, and CONTROL are all "artefacts" from QC. Remove them.
+# Bridge are the samples used for the bridging procedure. Remove them. 
+olinkdat <- olinkdat %>%
+  filter(cohort != 'Control',
+         cohort != 'CONTROL',
+         cohort != 'HOL-N',
+         cohort != 'HOL-F', 
+         cohort != 'Bridge')
+
+# TIEP and TIPSKC/ are spelling errors. Correct them. 
+olinkdat <- olinkdat %>%
+  mutate(cohort = case_when(cohort == 'TIEP' ~ 'TIPS',
+                            cohort == 'TIPSKC/' ~ 'TIPS',
+                            TRUE ~ cohort))
+
+# IDs ---- 
+# atm, id is a concatenated string of cohort and id (and sometimes sample info)
+# cohort already exists as a variable, so take that out
+# in the end we want a "CBMRID" which is cohort and padded id -sample -timepoint
+
+# following matches non-capturing group of letters at beginning of string
+# gsub with nothing to make newid
+olinkdat <- olinkdat %>%
+  mutate(newid = gsub("^(?:[A-Z]+)", "", SampleID))
+
+# for HOL samples, reverse the newid order, since they're reversed
+olinkdat <- olinkdat %>%
+  mutate(newid = case_when(cohort == 'HOL' ~ str_replace(newid,"([^-]+)-([^-]+)","\\2-\\1"),
+                           TRUE ~ newid))
+  
+# separate newid to get sample/time info
+olinkdat <- olinkdat %>%
+  separate(newid, into=c('newid','temp_sampleinfo'), extra='merge', sep='-')
+
+# and because TIPS isn't separated by a delimiter, extract with regex for TIPS
+olinkdat <- olinkdat %>%
+  mutate(temp_sampleinfo = case_when(cohort == 'TIPS' ~ gsub("[0-9]+", "", newid),
+                                     TRUE ~ temp_sampleinfo),
+         newid = case_when(cohort == 'TIPS' ~ gsub('[A-Z]+', '', newid),
+                           TRUE ~ newid))
+
+# and because HOL has some duplicated samples, drop those with the suffix _b
+# then drop the suffix 
+olinkdat <- olinkdat %>% 
+  filter(!(str_detect(newid, '_b') & cohort == 'HOL')) %>%
+  mutate(newid = case_when(cohort == 'HOL' & str_detect(newid, '_a') ~ str_replace(newid, '_a', ''),
+                           TRUE ~ newid))
+
+# pad the id str so that it's 3 digits (4 for ALD)
+olinkdat <- olinkdat %>%
+  mutate(newid = case_when(cohort == 'ALD' ~ str_pad(newid, width=4, side='left', '0'),
+                           cohort != 'ALD' ~ str_pad(newid, width=3, side='left', '0')))
+
+# and finally concat the newid and cohort
+olinkdat <- olinkdat %>%
+  unite('newid', c(cohort, newid), remove=FALSE, sep='')
+
+# SAMPLE TYPE AND TIMEPOINT ----
+# As the only cohort, TIPS only has sampletype 
+# As the only cohort, ALCO has BOTH timepoints and sampletype
+# The following have only timepoints: HCOT, HOL, MLGB, PRF, ROC, RFX
+# The following have no sample variables: ALD, HP
+olinkdat <- olinkdat %>%
+  mutate(sampletype = NA_character_,
+         timepoint = NA_character_) %>%
+  mutate(sampletype = case_when(cohort == 'TIPS' ~ temp_sampleinfo,
+                                TRUE ~ sampletype),
+         sampletype = case_when(cohort == 'ALCO' ~ str_extract(temp_sampleinfo, "S[0-9]+"),
+                                TRUE ~ sampletype)) %>%
+  mutate(timepoint = case_when(cohort == 'ALCO' ~ str_extract(temp_sampleinfo, 'T[0-9]+'),
+                               TRUE ~ timepoint),
+         timepoint = case_when(cohort == 'HCOT' | 
+                                 cohort == 'HOL' | 
+                                 cohort == 'MLGB' | 
+                                 cohort == 'PRF' | 
+                                 cohort == 'RDC' | 
+                                 cohort == 'RFX' ~ temp_sampleinfo,
+                               TRUE ~ timepoint)) %>%
+  select(-temp_sampleinfo)
+
+# cleanup timepoint notation for HCOT samples
+# add in baseline "timepoint" for HOL where visit number refers to visit after baseline
+olinkdat <- olinkdat %>%
+  mutate(timepoint = case_when(cohort == 'HCOT' & timepoint == 'A' ~ 'BL',
+                               cohort == 'HCOT' & timepoint == 'B' ~ 'OF',
+                               cohort == 'HCOT' & timepoint == 'C' ~ 'TR',
+                               cohort == 'HOL' & is.na(timepoint) ~ 'BL',
+                               TRUE ~ timepoint))
+
+# CBMR ID ----
+olinkdat <- olinkdat %>%
+  unite('CBMRID', c(newid,sampletype,timepoint), sep='-', na.rm=T, remove = F)
+
+# WIDE FORMAT ----
+olinkdat_wide <- olinkdat %>%
+  select(-c(OlinkID, UniProt, MissingFreq, Panel, Panel_Version, PlateID, QC_Warning, LOD, NPX, Normalization, Index, no_missing_obs, total_obs, propmissing, keep_assay)) %>%
+  pivot_wider(names_from = Assay, values_from = corrected_NPX)
+
+# SPLIT BY COHORT ----
+ setwd('PATH/Olink/Global_QC/split_cohorts/') # this is where your output goes
+
+olinkdat_wide %>%
+  group_split(cohort) %>%
+  sapply(., function (x) 
+    write.csv(x, file=paste0("QC_Olink", unique(x$cohort), "_wide.csv"), row.names=F))
+
+olinkdat %>%
+  group_split(cohort) %>%
+  sapply(., function (x) 
+    write.csv(x, file=paste0("QC_Olink", unique(x$cohort), "_long.csv"), row.names=F))
